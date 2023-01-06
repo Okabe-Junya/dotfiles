b4f2ac7d5915ca58eaeeb81b855f18462b1173cd
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2023-01-07 04:36:08 +0900

    pyenvの設定

--- .zsh/init/export.zsh
+++ .zsh/init/export.zsh
@@ -1,15 +1,28 @@
 export PS1="%~ "
+
+# Homebrew
 export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"
-eval "$(rbenv init -)"
+
 fpath=(~/.zsh $fpath)
 
+# llvm
 export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+# rbenv
+eval "$(rbenv init -)"
+
+# go
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
+
+# nvm
 export NVM_DIR="$HOME/.nvm"
-export LDFLAGS="-L/opt/homebrew/opt/zlib/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/zlib/include"
