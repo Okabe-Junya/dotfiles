225963263d205c6e834ab719b5274e121d989f8c
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2024-11-05 23:50:18 +0900

    chore: update install script

--- install/symlink.zsh
+++ install/symlink.zsh
@@ -1,9 +1,15 @@
 #!/usr/bin/env zsh
 
 # config
+if [ ! -d "${HOME}/.config" ]; then
+    mkdir "${HOME}/.config"
+fi
 ln -nfs "${HOME}/dotfiles/.config/git" "${HOME}/.config/git"
 ln -nfs "${HOME}/dotfiles/.config/git/.gitconfig" "${HOME}/.gitconfig"
 ln -nfs "${HOME}/dotfiles/.config/gh" "${HOME}/.config/gh"
 
 # zshrc
+if [ -f "${HOME}/.zshrc" ]; then
+    mv "${HOME}/.zshrc" "${HOME}/.zshrc.bak"
+fi
 ln -nfs "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
